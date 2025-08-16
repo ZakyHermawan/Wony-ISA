64c5314831591efbf8dda81180ea754e838b86e9
ZakyHermawan <zaky.hermawan9615@gmail.com>
2025-08-17 00:40:23 +0700

    [Wony] Fill subtarget info from table-gen into cpp file
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- llvm/lib/Target/Wony/WonySubtarget.cpp
+++ llvm/lib/Target/Wony/WonySubtarget.cpp
@@ -8,15 +8,16 @@
 #include "llvm/Target/TargetMachine.h"
 
 using namespace llvm;
+
+#define DEBUG_TYPE "wony-subtarget"
+
+#define GET_SUBTARGETINFO_TARGET_DESC
+#define GET_SUBTARGETINFO_CTOR
+#include "WonyGenSubtargetInfo.inc"
+
 // Pin the vtable to this file.
 void WonySubtarget::anchor() {}
 
 WonySubtarget::WonySubtarget(const Triple &TT, StringRef CPU, StringRef FS,
                                const TargetMachine &TM)
-    : TargetSubtargetInfo(TT, CPU, /*TuneCPU=*/"", FS, /*PN=*/{}, /*PF=*/{},
-                          /*PD=*/{},
-                          /*WPR=*/nullptr,
-                          /*WL=*/nullptr,
-                          /*RA=*/nullptr, /*IS=*/nullptr,
-                          /*OC=*/nullptr, /*FP=*/nullptr),
-      TLInfo(TM) {}
+    : WonyGenSubtargetInfo(TT, CPU, /*TuneCPU=*/"", FS), TLInfo(TM) {}
