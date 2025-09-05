bb09282ee8165b0c96a8a6e6475976f071f7f4fc
ZakyHermawan <zaky.hermawan9615@gmail.com>
2025-09-06 01:06:09 +0700

    [Wony] Add extld and movfromsp support to frame index elimination Implemenets the lowering of the frame indices for the extended loads and the movfromsp instructions.
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- llvm/lib/Target/Wony/WonyRegisterInfo.cpp
+++ llvm/lib/Target/Wony/WonyRegisterInfo.cpp
@@ -9,6 +9,7 @@
 #include "MCTargetDesc/WonyMCTargetDesc.h" // For the enum of the regclasses.
 #include "llvm/ADT/BitVector.h"
 #include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/TargetInstrInfo.h"
 #include "llvm/CodeGen/MachineFrameInfo.h"
 #include "llvm/CodeGen/TargetSubtargetInfo.h"
 
@@ -52,6 +53,7 @@ bool WonyRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator II,
   MachineBasicBlock &MBB = *MI.getParent();
   MachineFunction &MF = *MBB.getParent();
   const MachineFrameInfo &MFI = MF.getFrameInfo();
+  const TargetInstrInfo &TII = *MF.getSubtarget().getInstrInfo();
 
   DebugLoc DL = MI.getDebugLoc();
 
@@ -70,15 +72,50 @@ bool WonyRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator II,
   assert(FIOperandNum == 1 && "Stack argument is expected to be the second "
                               "operand for both loads and stores");
   switch (MI.getOpcode()) {
+  case Wony::LDRSEXTSP8:
+  case Wony::LDRZEXTSP8:
   case Wony::STRSP16:
   case Wony::LDRSP16:
   case Wony::STRSP32:
   case Wony::LDRSP32:
     FIOp.ChangeToRegister(Wony::SP, /*IsDef=*/false);
     Offset += MI.getOperand(2).getImm();
+    // If the offset doesn't fit, we need to expand into:
+    // off = ldimm offset
+    // base = movefromsp sp
+    // addr = addi16rr base, off
+    // loadOrStoreNoSP16 addr, 0
+    //
+    // SUBSP
+    // movfromsp
+    // loadOrStore
+    // ADDSP
     assert(Offset >= -64 && Offset < 63 && "Offset must fit 7 bits for now");
     MI.getOperand(2).setImm(Offset);
     break;
+  case Wony::MOVFROMSP: {
+    FIOp.ChangeToRegister(Wony::SP, /*IsDef=*/false);
+
+    // If the offset is zero, the MOVFROMSP is enough.
+    if (Offset == 0) {
+      break;
+    }
+    // Adapt the offset by change SP on the fly and back.
+    // ADDSP
+    // movfromsp
+    // SUBSP
+    // Note that there are other possible sequences to lower this offset, but
+    // this one doesn't require to use the register scavenger.
+    assert(Offset >= 0 && Offset <= 1023 && "offset out-of-range");
+    BuildMI(MBB, MI, DebugLoc(), TII.get(Wony::ADDSP), Wony::SP)
+        .addReg(Wony::SP, RegState::Kill)
+        .addImm(Offset);
+    BuildMI(MBB, std::next(MI.getIterator()), DebugLoc(), TII.get(Wony::SUBSP),
+            Wony::SP)
+        .addReg(Wony::SP, RegState::Kill)
+        .addImm(Offset);
+    break;
+  }
   default:
     llvm_unreachable("frame index used on unknown instruction");
   }
