7ce839284b08afaa78e0183cdaebbcd8600bc746
ZakyHermawan <zaky.hermawan9615@gmail.com>
2025-09-11 02:20:45 +0700

    [Wony] Implment the extended load for non-sp addressing mode
    Signed-off-by: ZakyHermawan <zaky.hermawan9615@gmail.com>

--- llvm/lib/Target/Wony/GISel/WonyInstructionSelector.cpp
+++ llvm/lib/Target/Wony/GISel/WonyInstructionSelector.cpp
@@ -136,7 +136,11 @@ bool WonyInstructionSelector::select(MachineInstr &I) {
 
 InstructionSelector::ComplexRendererFns
 WonyInstructionSelector::selectAddrMode(MachineOperand &Root) const {
-  if (!Root.isReg()) {
+  // Here we check if the SP addr mode applies before checking this one.
+  // Technically this is a hack because it is supposed to be handled at
+  // the .td level with the AddedComplexity. However TableGen does
+  // something unexpected with the AddedComplexity with extload.
+  if (selectSPAddrMode(Root) != std::nullopt) {
     return std::nullopt;
   }
 
